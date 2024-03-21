766dfeabf5fc3328a81064e2da8453cf91652870
david1ariel <david1ariel@gmail.com>
2024-03-21 11:13:13 +0200

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Index Page</h1>
+  <q-btn title="Release" label="Release" @click="router.push('/release')" />
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  name: "ReleasePage",
+  data() {
+    const currentLocation = ref(window.location)
+    const router = useRouter()
+    const quarantined = ref(false);
+    const recipient = ref(false);
+    const rtl = ref(false);
+    const dark = ref(false);
+    const password = ref("");
+    return {
+      router,
+      quarantined,
+      recipient,
+      rtl,
+      password,
+      dark,
+      currentLocation
+    };
+  },
+});
+</script>
