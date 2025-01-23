e7db3c51de3010499c199f75ebcb71d7e17ebd8e
David Ariel <david1ariel@gmail.com>
2025-01-23 11:38:31 +0200

    add reservedRights component

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@
     <!-- <Sidebar /> -->
 
     <RouterView />
+
+    <ReservedRights/>
   </main>
 </template>
 
 <script setup>
 import { useAppStateStore } from "./stores/appState";
 import LoadingComponent from "./components/BaseComponents/Loading/LoadingComponent.vue";
+import ReservedRights from "./components/BaseComponents/ReservedRights/ReservedRights.vue";
 
 const appState = useAppStateStore();
 
---
+++ src/components/BaseComponents/ReservedRights/ReservedRights.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="all-rights-wrapper">
+      <img class="mx-1" src="/src/assets/yazam-logo.png" alt="" />
+      <span class="fs-10 mx-1">All rights reserved YazamTech {{ year }} ©</span>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const year = ref(new Date().getFullYear());
+</script>
+<style scoped>
+.all-rights-wrapper {
+  display: flex;
+  margin-left: calc(50% - 200px);
+  width: 400px;
+  justify-content: center;
+  align-items: end;
+  position: fixed;
+  left: 2.5vw;
+  bottom: 15px;
+  img {
+    height: 20px;
+  }
+}
+</style>
